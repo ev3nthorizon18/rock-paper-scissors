afbb4ea27a97248ce25a88b5510a59b9e9ddab40
Jaime R <123219353+ev3nthorizon18@users.noreply.github.com>
2023-09-11 17:31:28 -0500

    Added an if else statement to playRound This was done to return the users choice of rock, paper, or scissors.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,28 @@
 //will create a rock, paper, and scissors game
-//The game will require a user input of the three choices (rock, paper, scissors).
+
 //Algorithm must accept user input without case sensitivity.
 //computer will choose at random from the three choices(rock, paper, scissors).
 //Algorithm will run 5 different games and display the winner after each game. 
+
+
+ //The game will require a user input of the three choices (rock, paper, scissors).
+playerSelection = prompt("Choose either Rock, Paper, or Scissors.");
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'Rock') {
+        return ('Rock');
+    }else if(playerSelection === 'Paper'){
+        return ('Paper');
+    }else{
+        return('Scissors');
+    }
+}
+
+
+ function getComputerChoice (Rock, Paper, Scissors) {}
+
+   
+  
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+
