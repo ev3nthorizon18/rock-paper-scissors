a2e8ed6e979565426abe05ff70eeeb8150eb0198
Jaime R <123219353+ev3nthorizon18@users.noreply.github.com>
2023-09-11 16:33:48 -0500

    Created index.html and rock-paper-scissors.js file Included the Docktype to the html file. Added a pseudocode to the rock-paper-scissors.js file. Included project details in the README.md file.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rock-paper-scissors
 Third Project: Creating the Rock Paper Scissors game. 
+
+First time fully using javascript to complete project. 
+Will focus on problem solving using pseudocode and then complete the algorithm to run in browser console.
---
+++ rock-paper-scissors.js
@@ -0,0 +1,5 @@
+//will create a rock, paper, and scissors game
+//The game will require a user input of the three choices (rock, paper, scissors).
+//Algorithm must accept user input without case sensitivity.
+//computer will choose at random from the three choices(rock, paper, scissors).
+//Algorithm will run 5 different games and display the winner after each game. 
