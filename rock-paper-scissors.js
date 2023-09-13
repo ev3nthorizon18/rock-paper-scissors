8115c25d52febec6329c26ae0993c8b0399deeb3
Jaime R <123219353+ev3nthorizon18@users.noreply.github.com>
2023-09-12 21:21:06 -0500

    Added game function to run the game 5 times.
    Included a for loop to accomplish this. Also adjusted the
    user input to give UpperCase strings to fix issue of calling
    the playRound function.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,50 +1,44 @@
-const playerSelection = prompt("Choose either Rock, Paper, or Scissors.").toLowerCase();
-    if (playerSelection === "rock") {
-        console.log("Rock");
-    }else if(playerSelection === "paper"){
-        console.log ("Paper");
-    }else if(playerSelection === "scissors"){
-        console.log ("Scissors");
-    }else{
-        console.log("Not an Option");
-    }
+const playerSelection = prompt("Choose either Rock, Paper, or Scissors.").toUpperCase();
+   
 //computer will choose at random from the three choices(rock, paper, scissors).
 
 
 function getComputerChoice () {
-    const rockPaperScissorsArray = ["Rock", "Paper", "Scissors"];
+    const rockPaperScissorsArray = ["ROCK", "PAPER", "SCISSORS"];
     const random = (Math.floor(Math.random()*rockPaperScissorsArray.length));
     return rockPaperScissorsArray[random]
 }
 
-
-//Algorithm will run paper rock scissors and display the winner after each game. 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection ==="rock" && computerSelection ==="Scissors"){
+    if (playerSelection ==="ROCK" && computerSelection ==="SCISSORS"){
         return("You win! Rock beats Scissors");
-    }else if(playerSelection ==="paper"&& computerSelection ==="Scissors"){
+    }else if(playerSelection ==="PAPER"&& computerSelection ==="SCISSORS"){
         return("You lose! Scissors beats Paper");
-    }else if(playerSelection ==="scissors" && computerSelection ==="Rock"){
+    }else if(playerSelection ==="SCISSORS" && computerSelection ==="ROCK"){
         return("You lose! Rock beats Scissors");
-    }else if(playerSelection ==="rock" && computerSelection ==="Paper"){
+    }else if(playerSelection ==="ROCK" && computerSelection ==="PAPER"){
         return("You lose! Paper beats Rock");
-    }else if(playerSelection ==="paper" && computerSelection ==="Rock"){
+    }else if(playerSelection ==="PAPER" && computerSelection ==="ROCK"){
         return("You win! Paper beats Rock");
-    }else if(playerSelection ==="scissors" && computerSelection ==="Paper"){
+    }else if(playerSelection ==="SCISSORS" && computerSelection ==="PAPER"){
         return("You win! Scissors beats Paper");
     }else {
         return("Game is a tie");
     }
 }
+//Algorithm will run paper rock scissors and display the winner after each game. 
 
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
-/*function game () {
+
+let computerSelection = getComputerChoice();
+
+
+function game () {
     for(let i=0; i<5; i++){
-        let result=playRound.call(this, playerSelection, computerSelection)
-        console.log(result)
+        let computerSelection = getComputerChoice();
+        let result=playRound.call(this,playerSelection, computerSelection);
+        if (result==="You win!")
+        console.log("Player Selection" + ": " + playerSelection + ", ", "Computer Selection" + ": " +computerSelection+ ", ", result)
     }
 }
 game()
- */
+ 
