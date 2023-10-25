97f66ac91f51a922535744a5e8b20d01b1fabf27
Jaime R <123219353+ev3nthorizon18@users.noreply.github.com>
2023-10-25 15:34:20 -0500

    Added three buttons to index.html included eventListener when clicking the three buttons

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,5 @@
 //prompts user to input a selection of rock, paper, or scissors and will uppercase the selection allowing the answer to be submitted in different case structure. 
-const playerSelection = prompt("Choose either Rock, Paper, or Scissors.").toUpperCase();
+// const playerSelection = prompt("Choose either Rock, Paper, or Scissors.").toUpperCase();
    
 //computer will choose at random from the three choices(rock, paper, scissors).
 function getComputerChoice () {
@@ -30,34 +30,41 @@ function playRound(playerSelection, computerSelection) {
 let computerSelection = getComputerChoice();
 
 //The game() function includes a for loop that will run the playRound 5 different times. Each run will determine a winner of the round. The winner is then tallied to determine who has the most wins in a best of five match.. 
-function game () {
-    let playerTally = 0;
-    let computerTally = 0;
-    for(let i=0; i<5; i++){
-        let computerSelection = getComputerChoice();
+// function game () {
+//     let playerTally = 0;
+//     let computerTally = 0;
+//     for(let i=0; i<5; i++){
+//         let computerSelection = getComputerChoice();
         
-        let result=playRound.call(this, playerSelection, computerSelection);
+//         let result=playRound.call(this, playerSelection, computerSelection);
 
-        console.log("Player Selection" + ": " + playerSelection + ", ", "Computer Selection" + ": " +computerSelection+ ", ", result);
+//         console.log("Player Selection" + ": " + playerSelection + ", ", "Computer Selection" + ": " +computerSelection+ ", ", result);
         
-// The if else statement tallies the wins of the user and the wins of the computer for the five game match. 
-        if (result==="You win! Scissors beats Paper" || result==="You win! Paper beats Rock" || result==="You win! Rock beats Scissors"){
-            playerTally +=1; 
-        }else if (result==="You lose! Scissors beats Paper" || result==="You lose! Paper beats Rock" || result==="You lose! Rock beats Scissors"){
-            computerTally+=1;
-        }
-    }   
+// // The if else statement tallies the wins of the user and the wins of the computer for the five game match. 
+//         if (result==="You win! Scissors beats Paper" || result==="You win! Paper beats Rock" || result==="You win! Rock beats Scissors"){
+//             playerTally +=1; 
+//         }else if (result==="You lose! Scissors beats Paper" || result==="You lose! Paper beats Rock" || result==="You lose! Rock beats Scissors"){
+//             computerTally+=1;
+//         }
+//     }   
 
 // The if else statement compares the playerTally to the computerTally to determine the winner of the five game match. 
-    console.log("Player Tally: " + playerTally, "Computer Tally: " +computerTally);
-if (playerTally>computerTally){
-        console.log("You win the best of five match! "+ playerTally + " to "+computerTally);
-}else if (computerTally>playerTally){
-        console.log("You lose the best of five match! "+ playerTally + " to "+computerTally);
-}else {
-            console.log("Best of five match is a tie!"+ playerTally + " to "+computerTally);
-     }     
+//     console.log("Player Tally: " + playerTally, "Computer Tally: " +computerTally);
+// if (playerTally>computerTally){
+//         console.log("You win the best of five match! "+ playerTally + " to "+computerTally);
+// }else if (computerTally>playerTally){
+//         console.log("You lose the best of five match! "+ playerTally + " to "+computerTally);
+// }else {
+//             console.log("Best of five match is a tie! "+ playerTally + " to "+computerTally);
+//      }     
+//     }
+// game()
+
+for(let i=0; i< button.length; i++){
+let button = document.querySelectorAll('button')[i];
+button.addEventListener('click', buttonClick)
+
+function buttonClick (e){
+    console.log(e.type);
     }
-game()
-
-
+}
