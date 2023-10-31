e4e95397cf4ce87fe68221640902e0f6daaf62be
Jaime R <123219353+ev3nthorizon18@users.noreply.github.com>
2023-10-30 19:35:19 -0500

    Created eListeners in Rock, Paper, Scissor buttons Added a div component into HTML file to store results

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,33 +1,60 @@
-//prompts user to input a selection of rock, paper, or scissors and will uppercase the selection allowing the answer to be submitted in different case structure. 
-// const playerSelection = prompt("Choose either Rock, Paper, or Scissors.").toUpperCase();
-   
-//computer will choose at random from the three choices(rock, paper, scissors).
+ //computer will choose at random from the three choices(rock, paper, scissors).
 function getComputerChoice () {
     const rockPaperScissorsArray = ["ROCK", "PAPER", "SCISSORS"];
     const random = (Math.floor(Math.random()*rockPaperScissorsArray.length));
     return rockPaperScissorsArray[random]
 }
 // Function playRound will compare the playerSelection with the computerSelection and determine a winner.
+
+const rockButton = document.getElementById("ROCK");
+const paperButton = document.getElementById("PAPER");
+const scissorsButton = document.getElementById("SCISSORS");
+
+function click(e){
+    const playerSelection = e.target.id;
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+}
+
+rockButton.addEventListener("click", click);
+paperButton.addEventListener("click", click);
+scissorsButton.addEventListener("click", click);
+
+
 function playRound(playerSelection, computerSelection) {
+
     if (playerSelection ==="ROCK" && computerSelection ==="SCISSORS"){
-        return("You win! Rock beats Scissors");
-    }else if(playerSelection ==="PAPER"&& computerSelection ==="SCISSORS"){
-        return("You lose! Scissors beats Paper");
-    }else if(playerSelection ==="SCISSORS" && computerSelection ==="ROCK"){
-        return("You lose! Rock beats Scissors");
-    }else if(playerSelection ==="ROCK" && computerSelection ==="PAPER"){
-        return("You lose! Paper beats Rock");
-    }else if(playerSelection ==="PAPER" && computerSelection ==="ROCK"){
-        return("You win! Paper beats Rock");
-    }else if(playerSelection ==="SCISSORS" && computerSelection ==="PAPER"){
-        return("You win! Scissors beats Paper");
+        console.log("You win! Rock beats Scissors");
+    }else if(playerSelection  ==="PAPER"&& computerSelection ==="SCISSORS"){
+        console.log("You lose! Scissors beats Paper");
+    }else if(playerSelection  ==="SCISSORS" && computerSelection ==="ROCK"){
+        console.log("You lose! Rock beats Scissors");
+    }else if(playerSelection   ==="ROCK" && computerSelection ==="PAPER"){
+        console.log("You lose! Paper beats Rock");
+    }else if(playerSelection  ==="PAPER" && computerSelection ==="ROCK"){
+        console.log("You win! Paper beats Rock");
+    }else if(playerSelection  ==="SCISSORS" && computerSelection ==="PAPER"){
+        console.log("You win! Scissors beats Paper");
     }else {
-        return("Game is a tie");
+       console.log("Game is a tie");
     }
 }
 
+
+function playerWins (){
+    ++playerWinsNumber;
+    
+}
+
+
+
+
+
+
+
+
 // This component will store the randomly selected string from rockPaperScissorsArray allowing the use of it for the future.
-let computerSelection = getComputerChoice();
+
 
 //The game() function includes a for loop that will run the playRound 5 different times. Each run will determine a winner of the round. The winner is then tallied to determine who has the most wins in a best of five match.. 
 // function game () {
@@ -58,13 +85,4 @@ let computerSelection = getComputerChoice();
 //             console.log("Best of five match is a tie! "+ playerTally + " to "+computerTally);
 //      }     
 //     }
-// game()
-
-for(let i=0; i< button.length; i++){
-let button = document.querySelectorAll('button')[i];
-button.addEventListener('click', buttonClick)
-
-function buttonClick (e){
-    console.log(e.type);
-    }
-}
+// game() 
